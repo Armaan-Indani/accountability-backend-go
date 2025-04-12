2f33775b0e3d4fd98cb9769cc861a67ce5f737b6
Armaan <indaniarmaan4@gmail.com>
2025-04-12 17:49:58 +0530

    feat: added occupation and about to user

--- model/user.go
+++ model/user.go
@@ -5,9 +5,11 @@ import "gorm.io/gorm"
 // User struct
 type User struct {
 	gorm.Model
-	Name      string     `gorm:"not null;size:50;" validate:"required,min=3,max=50" json:"name"`
-	Username  string     `gorm:"uniqueIndex;not null;size:50;" validate:"required,min=3,max=50" json:"username"`
-	Email     string     `gorm:"uniqueIndex;not null;size:255;" validate:"required,email" json:"email"`
-	Password  string     `gorm:"not null;" validate:"required,min=6,max=50" json:"password"`
-	TaskLists []TaskList `gorm:"foreignKey:UserID" json:"lists"`
+	Name       string     `gorm:"not null;size:50;" validate:"required,min=3,max=50" json:"name"`
+	Username   string     `gorm:"uniqueIndex;not null;size:50;" validate:"required,min=3,max=50" json:"username"`
+	Email      string     `gorm:"uniqueIndex;not null;size:255;" validate:"required,email" json:"email"`
+	Password   string     `gorm:"not null;" validate:"required,min=6,max=50" json:"password"`
+	Occupation string     `json:"occupation"`
+	About      string     `json:"about"`
+	TaskLists  []TaskList `gorm:"foreignKey:UserID" json:"lists"`
 }
